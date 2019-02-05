ffdde7ceeecdaf1c127e09fef46ab570b16beb13
t-kowalewski <wkowalewski@outlook.com>
2019-02-05 18:52:17 +0100

    code cleanup, added additional sound & animation funct

--- game.js
+++ game.js
@@ -1,26 +1,44 @@
+// Game data
 let userClickedPattern = [];
 let gamePattern = [];
-
 let buttonColors = ["red", "blue", "green", "yellow"];
 
-let randomChosenColor = buttonColors[nextSequence()];
+// User response / answer
+$(".btn").click(function(event) {
+    let userChosenColor = event.target.id;
+    userClickedPattern.push(userChosenColor);
 
-gamePattern.push(randomChosenColor);
-
-$("#" + randomChosenColor).fadeOut(100).fadeIn(100);
-
-// Play Button Sound
-let buttonSound = new Audio("sounds/" + randomChosenColor + ".mp3");
-buttonSound.play();
-
-
-// $(".btn").click(function(event) {
-//     let userChosenColor = event.id;
-// })
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+})
 
 
-// Function - generate random number
+// Function - Blink sequence / Game Mechanics
 function nextSequence() {
+    // Generate random number
     let randomNumber = Math.floor(Math.random() * 4);
-    return randomNumber;
+    let randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+
+    // Random button blink
+    $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
+
+    // Play random button sound
+    playSound(randomChosenColor);
+}
+
+// ===== Additional functions =====
+
+// Play button sound
+function playSound(name) {
+    let buttonSound = new Audio("sounds/" + name + ".mp3");
+    buttonSound.play();
+}
+
+// Button animation - "pressed" css class
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function() {
+        $("#" + currentColor).removeClass("pressed");
+    }, 100);
 }
