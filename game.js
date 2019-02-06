387899593ba8a91b96851c1fd9ace2974cc8add8
t-kowalewski <wkowalewski@outlook.com>
2019-02-06 02:21:14 +0100

    working game

--- game.js
+++ game.js
@@ -2,6 +2,21 @@
 let userClickedPattern = [];
 let gamePattern = [];
 let buttonColors = ["red", "blue", "green", "yellow"];
+let started = false;
+let level = 0;
+
+if (!started) {
+    // $(document).on("keypress", nextSequence);
+
+    $(document).on("keydown", function(event) {
+        if (event.key === "a" || event.key === "A") {
+            $("#level-title").text("Level " + level);
+            nextSequence();
+            started = true;
+        }
+    });
+}
+
 
 // User response / answer
 $(".btn").click(function(event) {
@@ -10,11 +25,18 @@ $(".btn").click(function(event) {
 
     playSound(userChosenColor);
     animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length - 1);
 })
 
 
-// Function - Blink sequence / Game Mechanics
+// Function - Game Mechanics / Blink sequence
 function nextSequence() {
+    userClickedPattern = [];
+
+    // Increase level
+    level++;
+    $("#level-title").text("Level " + level);
+
     // Generate random number
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColor = buttonColors[randomNumber];
@@ -27,9 +49,36 @@ function nextSequence() {
     playSound(randomChosenColor);
 }
 
+
+// Function - Check user answer
+function checkAnswer(currentLevel) {
+    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+        console.log("right");
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function () {
+                nextSequence()
+            }, 1000);
+        }
+    }
+    // 
+    else {
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function () {
+            $("body").removeClass("game-over");
+        }, 200);
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        
+        startOver();
+        console.log("wrong");
+    }
+    console.log("user clicks", userClickedPattern);
+    console.log("game clicks", gamePattern);
+}
+
 // ===== Additional functions =====
 
-// Play button sound
+// Play button/wrong sound
 function playSound(name) {
     let buttonSound = new Audio("sounds/" + name + ".mp3");
     buttonSound.play();
@@ -41,4 +90,11 @@ function animatePress(currentColor) {
     setTimeout(function() {
         $("#" + currentColor).removeClass("pressed");
     }, 100);
+}
+
+// Start over
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
 }
